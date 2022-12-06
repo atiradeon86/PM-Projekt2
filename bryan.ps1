4839c3b0dd155017d0399ab23dd6bcb23d678c86
Attila Horvath <atiradeon86@hotmail.com>
2022-12-06 16:25:58 +0100

    v01

--- bryan.ps1
+++ bryan.ps1
@@ -9,67 +9,115 @@ $hostname= hostname
 $username= whoami
 $log_file= $pwd.Path + "\$log_name.txt"
 $passwd = "1234" | ConvertTo-SecureString -AsPlainText -Force
+$fqdn= [System.Net.Dns]::GetHostByName($env:computerName).HostName;
+
+Start-Transcript -path "$log_file" -append 
 
 
 #Functions 
-
 Function Check {
 
 [bool] $ok = 0
 
 #Admin?   
+
 $admin_check = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
 
 if ($admin_check -eq $true) {
-    echo "Admin Ok"
     [bool] $admin_check = 1
+
+    #Allow unsigned PS1 file to running
+    Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 } else {
-    echo "Please run as Administrator ..."
+    echo "Please run as Administrator -> Bye..."
 }
+
+#Internet Connection Testing
+
+[bool] $NetConnection_check = 0
+$Test_NetConnection = Test-NetConnection -Port 80 -InformationLevel 'Detailed'
+
+if ($Test_NetConnection.TcpTestSucceeded -eq $true) {
+    [bool] $NetConnection_check = 1
+} else {
+    echo "You need internet access -> Bye..."
+}
+
+#.Net Version Check
 
 $version = (Get-ItemProperty "HKLM:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full").Release
 
-if ($version -eq 533320) {
+[bool] $dotnet_check = 0
+
+if ( ($version -gt 461814) -or ($version -eq 461814) ) {
     [bool] $dotnet_check = 1
-    echo ".Net: 4.8.1 Installed"
+} else {
+    echo "Please install .Net version at least 4.7.2 (Your installed build number: $version)"
 }
 
-if (($admin_check -eq $true) -and ($dotnet_check -eq $true))  {
-    echo "Ok"
+if (($admin_check -eq $true) -and ($dotnet_check -eq $true) -and ($NetConnection_check -eq $true))  {
+    Write-Host "`r`n"
+    Write-Host "Checking for requierments:`r`n" -ForegroundColor Red
+    Write-Host "Admin -> Ok" -ForegroundColor Green
+    Write-Host "Internet Connection -> Ok" -ForegroundColor Green
+    Write-Host ".Net: Version -> Ok" -ForegroundColor Green
+    Write-Host "`r`n"
+    Write-Host "Done ..." -ForegroundColor Green
+    Write-Host "`r`n"
+
 } else {
     exit
 }
 
 }
-Check
+
 function CreatFolders {
     param (
-        [string[]]$file,  [string[]]$disk
+        [string[]]$file
     )
 
     $data= Import-Csv -Path $file
-    $InitialPath= "$disk`:`\Teszt" 
-    echo $InitialPath
-    
-    $data | ForEach-Object {
-        $CurrentFolder = $_.Name
-        $CurrentFolder = Join-Path -Path "$InitialPath" -ChildPath "$CurrentFolder"
-        New-Item -Path $CurrentFolder  -ItemType Directory
-    } 
-        
+
+    $Disk= Get-Disk | Sort-Object -Property Size -desc | Select-object -First 1   
+    $Disk_number= $Disk.Number
+    $Partition= Get-Partition -DiskNumber $Disk_number | Sort-Object "Size" -Descending | Select-object -First 1
+    $Drive_Letter = $Partition.DriveLetter
+    $InitialPath= "$Drive_Letter`:`\Teszt" 
+     
+    $chk = Test-Path -Path $InitialPath
+
+   
+        $data | ForEach-Object {
+                $CurrentFolder = $_.Name
+                $CurrentFolder = Join-Path -Path "$InitialPath" -ChildPath "$CurrentFolder"
+                New-Item -Path $CurrentFolder  -ItemType Directory
+            } 
+         
+    $BeforeFolder = $pwd
+    Set-Location $InitialPath
+    $folders =  Get-ChildItem -Directory -Depth 0
+    $arr =@($folders.Name)
+
+    for ($i=0; $i -lt $arr.Length; $i++) {
+        $CompFolder = $InitialPath + "\" + $arr[$i]
+        New-SmbShare -Name $arr[$i] -Path "$CompFolder" -ea 0
     }
+    
+    Set-Location $BeforeFolder
+    
+}
     
 Function Auth {
 
     #1. Step Create Credential Object
-    <#
+    
     [string]$userName = 'bryan'
     [string]$userPassword = '1234'
     
     [securestring]$secStringPassword = ConvertTo-SecureString $userPassword -AsPlainText -Force
     [pscredential]$credObject = New-Object System.Management.Automation.PSCredential ($userName, $secStringPassword)
     $credObject | Export-CliXml  -Path bryan-cred.xml
-    #>
+    
 
     #$Import existing Credential file
     $credential = Import-CliXml -Path .\bryan-cred.xml
@@ -77,10 +125,10 @@ Function Auth {
     $UnsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 
     #Create temporary Credential file
-    [string]$userName = 'bryan'
-    $pwd = Read-Host "Please enter your password" -AsSecureString
-    [pscredential]$credObject = New-Object System.Management.Automation.PSCredential ($userName, $pwd)
-    $credObject | Export-CliXml  -Path temp-cred.xml
+    [string]$t_userName = 'bryan'
+    $t_pwd = Read-Host "Please enter your password" -AsSecureString
+    [pscredential]$t_credObject = New-Object System.Management.Automation.PSCredential ($t_userName, $t_pwd)
+    $t_credObject | Export-CliXml  -Path temp-cred.xml
 
     $t_credential = Import-CliXml -Path .\temp-cred.xml
     $bstr_temp = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($t_credential.password)
@@ -89,25 +137,13 @@ Function Auth {
     #Compare unencrypted strings ... :)
     if (-not( $UnsecurePassword -eq $T_UnsecurePassword )) {
        del .\temp-cred.xml
+       del .\bryan-cred.xml
        echo "Bad Password! Bye ..."
        exit
     }
     del .\temp-cred.xml
+    del .\bryan-cred.xml
 }
-
-function Transcript { 
-    param (
-        [string[]]$cmd
-    )
-    if ($cmd -eq "start") {
-    Start-Transcript -path "$log_file" -append 
-    } else {
-        Stop-Transcript    
-    }
-
-}
-
-#Transcript -cmd start
 function Data-show{
 
     echo "**********************"
@@ -120,20 +156,83 @@ function Data-show{
     echo "**********************"
     
 }
-Auth
+function  Myapp{
 
-Data-show
+$Disk= Get-Disk | Sort-Object -Property Size -desc | Select-object -First 1   
+$Disk_number= $Disk.Number
+$Partition= Get-Partition -DiskNumber $Disk_number | Sort-Object "Size" -Descending | Select-object -First 1
+$Drive_Letter = $Partition.DriveLetter
+$InitialPath= "$Drive_Letter`:`\Teszt" 
 
-function ChooseBestDisk{ 
+$chk = Test-Path -Path "$InitialPath\pm-control.zip" -PathType Leaf
 
-    $Disk= Get-Disk | Sort-Object -Property Size -desc | Select-object -First 1   
-    $Disk_number= $Disk.Number
-    $Partition= Get-Partition -DiskNumber $Disk_number | Sort-Object "Size" -Descending | Select-object -First 1
-    $Drive_Letter = $Partition.DriveLetter
-    #return $Drive_Letter
+echo $InitialPath
+
+if ($chk -eq $false) {
+
+wget https://pm.bryan86.hu/pm-control.zip -OutFile "$InitialPath\pm-control.zip"
+
+    
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
-#ChooseBestDisk
 
-#CreatFolders -file folders.csv -disk $Drive_Letter
+Unzip "$InitialPath\pm-control.zip" "$InitialPath\PM"
+echo $pwd
+}
 
-#Transcript -cmd stop
+$fqdn > $InitialPath\PM\Config.txt
+
+#Register Event Source
+[System.Diagnostics.EventLog]::CreateEventSource("Bryan PM Control", "Application")
+
+#Create ScheduledTask
+
+$action = New-ScheduledTaskAction -Execute "$InitialPath\PM\pmc.exe"
+$trigger = New-ScheduledTaskTrigger -AtStartup
+$settings = New-ScheduledTaskSettingsSet
+$task = New-ScheduledTask -Action $action  -Trigger $trigger -Settings $settings
+Register-ScheduledTask PMControl -InputObject $task 
+Set-ScheduledTask -TaskName 'PMControl'
+
+
+#Create ScheduledTask Trigger
+$class = cimclass MSFT_TaskEventTrigger root/Microsoft/Windows/TaskScheduler
+$trigger = $class | New-CimInstance -ClientOnly
+$trigger.Enabled = $true
+$trigger.Subscription = ' <QueryList><Query Id="0" Path="Application"><Select Path="Application">*[System[Provider[@Name=''Bryan PM Control''] and EventID=3001]]</Select></Query></QueryList>'
+
+$ActionParameters = @{
+    Execute  = "$InitialPath\PM\pmc.exe"
+}
+
+$Action = New-ScheduledTaskAction @ActionParameters
+$Settings = New-ScheduledTaskSettingsSet
+
+$RegSchTaskParameters = @{
+    TaskName    = 'Bryan PM Control - Trigger'
+    Description = 'Bryan PM Control - Eventlog Trigger'
+    TaskPath    = '\Event Viewer Tasks\'
+    Action      = $Action
+    Settings    = $Settings
+    Trigger     = $Trigger
+}
+
+Register-ScheduledTask @RegSchTaskParameters
+
+#Test -> Write-EventLog -ComputerName "$env:computername" -LogName Application -Source "Bryan PM Control" -EventID 3001 -Message "New Message for Task Scheduler"
+}
+
+#Workflow
+
+Check
+Data-show
+CreatFolders -file folders.csv
+Myapp
+Auth
+CreatFolders -file folders2.csv
+
+Stop-Transcript
